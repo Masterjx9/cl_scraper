6fedac98780d23a651ec003e4bc0537addf9885a
Jaton Justice <masterjx9@gmail.com>
2023-05-31 02:45:51 -0700

    fixed port for fly.io

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def home():
 
     return render_template('index.html', data=data)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080", threaded=True)
